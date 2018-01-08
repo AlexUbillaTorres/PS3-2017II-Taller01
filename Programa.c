2b10a46c8bd79d18fbd1d3a9783f4d1481e38fcc
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2018-01-08 20:33:35 +0000

    Update Programa.c

--- Programa.c
+++ Programa.c
@@ -4,17 +4,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-//#include "csapp.h"
 
 #define DEF_MODE   S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH
 #define DEF_UMASK  S_IWGRP|S_IWOTH
 
-int main()
+int main(int argc, char*argv[])
 {
 
 int fd;
 
-fd = open("prueba.txt", O_RDWR|O_CREAT, DEF_MODE);
+fd = open(argv[1], O_RDWR|O_CREAT, DEF_MODE);
 write(fd,"prueba",6);
 
 close(fd);
@@ -22,5 +21,4 @@ close(fd);
 exit(0);
 
 }
-
 
